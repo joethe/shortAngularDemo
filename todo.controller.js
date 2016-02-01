9d97b85a5bba1e79ad16a4225a0279d26425e334
Joseph Thelen <joethe@users.noreply.github.com>
2016-02-01 13:40:03 -0600

    Added default list value, and a function to add things to the list

--- todo.controller.js
+++ todo.controller.js
@@ -1,6 +1,16 @@
-angular.module("todoApp").controller("todoController", function() {
+angular.module("main").controller("todoController", function() {
   var todoList = this;
   
   var todoList.todos = [
-  ]
+    {item:"attend 3601", done:true},
+    {item:"learn angular", done:false},
+    {item:"make websites", done:false},
+    {item:"??????", done:false},
+    {item:"profit", done:false}
+  ];
+  
+  todoList.addTodo = function() {
+    todoList.todos.push({text: todoList.todoText, done: false});  // put the new item in the list
+    todoList.todoText = ''; // resetting the input field
+  };
 });
