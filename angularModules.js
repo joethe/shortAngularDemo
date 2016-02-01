20854383088e0b4b9cfbe793bec3264f21a208bc
Joseph Thelen <joethe@users.noreply.github.com>
2016-02-01 13:36:48 -0600

    added more modules

--- angularModules.js
+++ angularModules.js
@@ -1,1 +1,4 @@
-angular.module("todoApp", []);
+
+angular.module("services", []);        // module to contain services controllers
+angular.module("controllers", []);     // module to contain general purpose controllers
+angular.module("main", ["services"]);  // main module for our todoList app
